531545b4f13e78a29a7e91b73aa4b9a53060bacb
DARSHIT TARAPARA <94553274+Darshittarapara@users.noreply.github.com>
2022-04-30 16:40:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+let imgnone =document.getElementById('img-none');
+console.log(imgnone)
+
+
+let displayblock=document.getElementById('target');
+ 
+const displayreview=()=>{    
+displayblock.classList.toggle('silde-block');
+imgsrc()
+style()
+}
+const style=()=>{
+    if(document.getElementById('displaycontain').style.background!="red"){
+        document.getElementById('displaycontain').style.background="red"
+        document.getElementById('displaycontain').style.color='white'
+    }
+    else{
+        document.getElementById('displaycontain').style.background="white"
+        document.getElementById('displaycontain').style.color='black'
+    }
+
+}
+const imgsrc=()=>{
+ if(imgnone.getAttribute('class')=='fa-solid fa-angle-up'){
+     imgnone.setAttribute('class','fa-solid fa-angle-down')
+ }
+ else {
+    imgnone.setAttribute('class','fa-solid fa-angle-up')
+
+ }
+}
+document.getElementById('displaycontain').addEventListener('click',displayreview);
+
+let menu =document.getElementById("menu");
+let navbar =document.getElementById("navbar");
+
+const display =()=>{
+    navbar.classList.toggle("show");
+}
+menu.addEventListener("click",display)
+document.getElementById("close").addEventListener("click",display);
+
+
